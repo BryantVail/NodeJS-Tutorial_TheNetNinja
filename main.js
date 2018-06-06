dc10a8c186a3bf687a04a52c0108b2c3742724a2
Bryant Vail <Bryanttv@outlook.com>
2018-06-06 18:33:08 -0400

    done

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
 //main.js
 //#5, FunctionExpressions
 
+//normal function statement
+function sayHi(){
+    console.log("Hi");
+}
 
+sayHi();
 
+//Function Expression
+var sayBye = function(){
+    console.log("Bye");
+}//end sayBye
 
+sayBye();
 
+//call function
+function callFunction(fn){
+    fn();
+}
+
+callFunction(sayBye);
 
 
 
