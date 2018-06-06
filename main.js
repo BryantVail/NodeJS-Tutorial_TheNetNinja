44cde5b4d75874e51abec8b4fdba78eb98cc90ca
Bryant Vail <Bryanttv@outlook.com>
2018-06-06 18:26:47 -0400

    init

--- main.js
+++ main.js
@@ -1,24 +1,9 @@
 //main.js
+//#5, FunctionExpressions
 
-//console.log("hey there");
 
-// setTimeout(function(){
-//     console.log("three seconds have passed.");
-// }, 3000);
 
-// let time = 0;
-// let timer = setInterval(function(){
-//     time += 2;
-//     console.log(`${time} seconds have passed.`)
-//     if(time>5){
-//         clearInterval(timer);
-//     }
-// }, 2000);
 
-// 'dirname', 'filename'
-
-console.log(__dirname);
-console.log(__filename);
 
 
 
