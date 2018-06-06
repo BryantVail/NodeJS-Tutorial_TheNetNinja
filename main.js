1befaf9ceea35459fbbb58cd0f107a775b60e17a
Bryant Vail <Bryanttv@outlook.com>
2018-06-06 18:23:19 -0400

    __dirname , __filename

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 //main.js
 
-console.log("hey there");
+//console.log("hey there");
 
+// setTimeout(function(){
+//     console.log("three seconds have passed.");
+// }, 3000);
 
+// let time = 0;
+// let timer = setInterval(function(){
+//     time += 2;
+//     console.log(`${time} seconds have passed.`)
+//     if(time>5){
+//         clearInterval(timer);
+//     }
+// }, 2000);
 
+// 'dirname', 'filename'
 
-
+console.log(__dirname);
+console.log(__filename);
 
 
 
